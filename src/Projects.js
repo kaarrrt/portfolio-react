57a960313c5757717352175990883dcf0c571645
Karthik <karthikm7611@gmail.com>
2022-07-10 18:29:46 +0530

    Project Container Changes

--- src/Projects.js
+++ src/Projects.js
@@ -5,12 +5,13 @@ import ProjectCard from './ProjectCard.js'
 function Projects() {
   return (
     <div className="project">
+        <div className="ptitle">Projects</div>
         <div className="project-ctn">
           <ProjectCard img={ecomm} title={"Ecommerce Development"}/>
-          <ProjectCard img={ecomm} title={"Ecommerce Development"}/>
+          <ProjectCard img={ecomm} title={"Cab Management System"}/>
           <ProjectCard img={ecomm} title={"Ecommerce Development"}/>
         </div>
-        <div className="ptitle">Projects</div>
+        
     </div>
   )
 }
