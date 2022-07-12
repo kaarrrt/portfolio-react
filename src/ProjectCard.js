6de82f1e3d39a92d1e68492cc5eed498f4e86bdb
Karthik <karthikm7611@gmail.com>
2022-07-12 22:50:54 +0530

    alt tag

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -5,8 +5,8 @@ import './ProjectCard.css';
 function ProjectCard(props) {
   return (
     <div className="card-ctn">
-            <img src={pc} className="pc" />
-            <img src={props.img} className="pimg" />
+            <img src={pc} className="pc" alt="pc"/>
+            <img src={props.img} className="pimg" alt="img"/>
         <h1 className="title">{props.title}</h1>
         <button className="ghub"><img src={git} alt="" /></button>
         <p className="desc"></p>
