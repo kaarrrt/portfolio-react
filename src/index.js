3990e4f1b8247798fd19c12ec8626479ec727c9f
Karthik <karthikm7611@gmail.com>
2022-07-13 19:27:13 +0530

    removed project import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Projects from './Projects'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
