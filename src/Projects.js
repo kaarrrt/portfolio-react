2d92d3f0bb75d61a93d6b2180352f42fd920be75
Karthik <karthikm7611@gmail.com>
2022-07-16 22:34:34 +0530

    added alt

--- src/Projects.js
+++ src/Projects.js
@@ -11,11 +11,11 @@ function Projects() {
         <div className="img-overlays">
           <img src={ecomm} alt="" className="ecomm"  id='ecom'/>
           <div className="overlay1">
-            <a href='https://github.com/kaarrrt/amazon-clone' ><button className="git-btn"><img src={git}  className="git-image"/></button></a>
+            <a href='https://github.com/kaarrrt/amazon-clone' ><button className="git-btn"><img src={git}  className="git-image" alt="clone"/></button></a>
           </div>
           <img src={cabs} alt="" className="cabs" />
           <div className="overlay2">
-            <a href="https://github.com/kaarrrt/DBMS-project"><button className="git-btn" ><img src={git}  className="git-image"/></button></a>
+            <a href="https://github.com/kaarrrt/DBMS-project"><button className="git-btn" ><img src={git}  className="git-image" alt="cabs-image"/></button></a>
           </div>
           
         </div>
